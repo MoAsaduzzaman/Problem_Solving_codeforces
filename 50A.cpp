8c15f073a7e8b766f08bfbcabb05eeb6fce81c60
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-14 01:00:50 +0600

    Domino pilling

---
+++ 50A.cpp
@@ -0,0 +1,13 @@
+//Problem name: 50A
+//https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int M,N;
+    cin >> M >> N;
+    cout << (M*N)/2 << endl;
+    
+    return 0;
+}
