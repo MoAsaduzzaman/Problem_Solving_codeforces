6a8606d60082acc324e8c00faafc9f9324ddb13a
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-15 00:28:57 +0600

    Bit++

---
+++ 282A.cpp
@@ -0,0 +1,25 @@
+//Problem name: 282A
+//https://codeforces.com/problemset/problem/282/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, x=0;
+    cin >> n;
+    string s;
+    while(n--){
+        cin >> s;
+        if (s[1] == '+')
+        {
+            ++x;
+        }
+        else
+        {
+            --x;
+        }
+    }
+
+    cout << x << endl;
+    
+    return 0;
+}
