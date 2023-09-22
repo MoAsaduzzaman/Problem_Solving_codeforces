79a729a615982ad18fbe3a1de31d11d503f20b2f
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-23 00:17:50 +0600

    To My Critics

---
+++ 1850A.cpp
@@ -0,0 +1,18 @@
+//Problem name: 1850A
+//https://codeforces.com/contest/1850/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,a,b,c;
+    cin >> t;
+    while(t--){
+        cin >> a >> b >> c;
+        if((a+b)>=10 || (a+c)>=10 || (b+c)>=10){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
