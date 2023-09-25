65b08c5f29afe752abc4b9112b23325a82206e51
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-26 00:09:15 +0600

    Increasing Sequence

---
+++ 1882A.cpp
@@ -0,0 +1,32 @@
+//Problem name: 1882A
+//https://codeforces.com/contest/1882/problem/A
+
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main(){
+    int test_case,n;
+    cin >> test_case;
+
+    while(test_case--){
+        cin >> n;
+        vector <int> asad(n);
+
+        for(int i=0; i<n; i++)
+            cin >> asad[i];
+            int cnt =1;
+        for(int i=0; i<n; i++){
+            if(i==0){
+                if(asad[i]==cnt)
+                    cnt++;
+                    continue;
+            }
+            cnt++;
+            if(asad[i]==cnt)
+                cnt++;
+        }
+        cout << cnt << endl;
+        test_case--;
+    }
+    return 0;
+}
