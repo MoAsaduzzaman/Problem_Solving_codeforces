18e7b5383f969e555cee62fdbfef9b0b05662bfc
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-16 00:13:08 +0600

    Again Twenty Five!

---
+++ 630A.cpp
@@ -0,0 +1,13 @@
+//Problem name: 630A
+//https://codeforces.com/problemset/problem/630/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    cout << 25 << endl;
+
+    return 0;
+}
