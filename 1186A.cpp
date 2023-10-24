5e1da3bd2ff0af5307d034c7de82a15d2919d239
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-24 16:45:44 +0600

    Vus the Cossack and a Contest

---
+++ 1186A.cpp
@@ -0,0 +1,15 @@
+//Problem name: 1186A
+//https://codeforces.com/problemset/problem/1186/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,k;
+    cin >> n >> m >> k;
+    if(n>m || n>k){
+        cout << "No" << endl;
+    }else{
+        cout << "Yes" << endl;
+    }
+    return 0;
+}
