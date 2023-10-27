fb82b5c9ea57317892956f28a4e3e12d41682d06
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-28 00:04:04 +0600

    Beat the Odds

---
+++ 1691A.cpp
@@ -0,0 +1,24 @@
+//Problem name: 1691A
+//https://codeforces.com/problemset/problem/1691/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector <int> a(n);
+
+        int cnt = 0;
+    
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+            if(a[i]%2==0)
+                cnt++;
+        }
+        cout << min(cnt,(n-cnt)) << endl;
+    }
+    return 0;
+}
