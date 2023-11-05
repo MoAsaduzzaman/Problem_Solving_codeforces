d95ef1fd62e9b0df639fdf5af5bab57720f2f5a5
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-05 12:25:16 +0600

    Strange Functions

---
+++ 1455A.cpp
@@ -0,0 +1,15 @@
+//Problem name: 1455A 
+//https://codeforces.com/problemset/problem/1455/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << s.length() << endl;
+    }
+    return 0;
+}
