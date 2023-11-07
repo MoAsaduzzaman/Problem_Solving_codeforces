8ddb3ceb684aa618d759c40ab78ba016a43cebcf
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-07 11:02:48 +0600

    Two Regular Polygons

---
+++ 1312A.cpp
@@ -0,0 +1,20 @@
+//Problem name: 1312A 
+//https://codeforces.com/problemset/problem/1312/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+
+        if(n%m==0){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0; 
+}
