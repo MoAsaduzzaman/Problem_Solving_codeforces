27f706a45fdf0655f2f6da9d77d08450c5d01139
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-08 11:47:36 +0600

    Secret Sport

---
+++ 1894A.cpp
@@ -0,0 +1,17 @@
+//Problem name: 1894A 
+//https://codeforces.com/contest/1894/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin >>n;
+    string s;
+    cin >> s;
+    cout << s[s.length()-1]<<endl;
+  }
+  return 0;
+}
