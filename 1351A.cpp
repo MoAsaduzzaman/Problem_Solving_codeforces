66e268fdb0ccd77c4e8bba45a35c2fae5b138a2e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-12 09:28:38 +0600

    A+B (Trial Problem)

---
+++ 1351A.cpp
@@ -0,0 +1,15 @@
+//Problem name: 1351A
+//https://codeforces.com/problemset/problem/1351/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    return 0;
+}
