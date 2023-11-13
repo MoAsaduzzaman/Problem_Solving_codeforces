54b931a1176c181e48df45117bb8f36526b35248
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-13 07:55:03 +0600

    Dawid and Bags of Candies

---
+++ 1230A.cpp
@@ -0,0 +1,29 @@
+//Problem name: 1230A
+//https://codeforces.com/problemset/problem/1230/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a1,a2,a3,a4;
+    cin >> a1 >> a2 >> a3 >> a4;
+
+    if((a1+a2)==(a3+a4)){
+        cout << "YES" << endl;
+    }else if((a1+a3)==(a2+a4)){
+        cout << "YES" << endl;
+    }else if((a1+a4)==(a2+a3)){
+        cout << "YES" << endl;
+    }else if(a1==a2+a3+a4){
+        cout << "YES" << endl;
+    }else if(a2==a1+a3+a4){
+        cout << "YES" << endl;
+    }else if(a3==a1+a2+a4){
+        cout << "YES" << endl;
+    }else if(a4==a1+a2+a3){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
