dc48e35b89c3cae30e5926c0d4750ce5a98c4a38
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-14 08:03:35 +0600

    Rainbow Dash, Fluttershy and Chess Coloring

---
+++ 1393A.cpp
@@ -0,0 +1,15 @@
+//Problem name: 1393A  
+//https://codeforces.com/problemset/problem/1393/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        cout << (n/2)+1 << endl;
+    }
+    return 0;
+}
