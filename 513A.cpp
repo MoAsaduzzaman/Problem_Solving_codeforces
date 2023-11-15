fa7e25dd76ec47fa44b0aee05ef48c5656d1dca7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-15 09:00:59 +0600

    Game

---
+++ 513A.cpp
@@ -0,0 +1,17 @@
+//Problem name: 513A 
+//https://codeforces.com/problemset/problem/513/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n1,n2,k1,k2;
+    cin >> n1 >> n2 >> k1 >> k2;
+
+    if(n1>n2){
+        cout << "First" << endl;
+    }else{
+        cout << "Second" << endl;
+    }
+
+    return 0;
+}
