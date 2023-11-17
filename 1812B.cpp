64cea5e2ad9612bf220084301bd4d207cdcfe4c4
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-17 08:14:41 +0600

    Was it Rated?

---
+++ 1812B.cpp
@@ -0,0 +1,17 @@
+//Problem name: 1812B 
+//https://codeforces.com/contest/1812/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    if(n==15 || n==20 || n==21){
+        cout << "NO" << endl;
+    }else{
+        cout << "YES" << endl;
+    }
+
+    return 0;
+}
