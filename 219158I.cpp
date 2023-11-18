085a962a80a946a053826ee74fb9b64acfc5d6f9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-19 00:25:29 +0600

    Welcome for you with Conditions

---
+++ 219158I.cpp
@@ -0,0 +1,17 @@
+//Problem name: 219158I 
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B;
+    cin >> A >> B;
+
+    if(A>=B){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
