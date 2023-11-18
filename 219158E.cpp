937ce37f4e886fbd02b9f6a60e95593743d48be6
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-19 00:24:48 +0600

    Area of a Circle

---
+++ 219158E.cpp
@@ -0,0 +1,15 @@
+//Problem name: 219158E 
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi = 3.141592653;
+    double R;
+
+    cin >> R;
+
+    cout << fixed << setprecision(9) << pi*(pow(R,2)) << endl;
+
+    return 0;
+}
