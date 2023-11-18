4abe0e51bb2c23c7dfd0939082833c72b1bcbbf5
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-19 00:24:04 +0600

    Say Hello With C++

---
+++ 219158A.cpp
@@ -0,0 +1,13 @@
+//Problem name: 219158A
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string S;
+    cin >> S;
+
+    cout << "Hello, " << S << endl; 
+
+    return 0;
+}
