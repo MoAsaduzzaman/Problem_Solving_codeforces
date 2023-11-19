3aaafd28680a8ca95b4a5d9183d1f75355edbace
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-19 12:19:15 +0600

    GCD

---
+++ 219432L.cpp
@@ -0,0 +1,12 @@
+//Problem name: 219432L 
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/L
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B;
+    cin >> A >> B;
+    int ans = __gcd(A,B);
+    cout << ans << endl;
+    return 0;
+}
