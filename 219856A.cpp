a926a4b6f585667dc514b2ecda9cd92643089ebe
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-21 00:05:33 +0600

    Create A New String

---
+++ 219856A.cpp
@@ -0,0 +1,17 @@
+//Problem name: 219856A 
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string S,T;
+    cin >> S >> T;
+
+    int Slen = S.length();
+    int Tlen = T.length();
+
+    cout << Slen << " " << Tlen << endl;
+    cout << S << " " << T << endl;
+
+    return 0;
+}
