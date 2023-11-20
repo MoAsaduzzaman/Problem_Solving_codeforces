2fcf31e08653e0d5c6cf9ab55a6b0889e73537b4
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-20 11:09:41 +0600

    Basic Data Types

---
+++ 219158B.cpp
@@ -0,0 +1,22 @@
+//Problem name: 219158B 
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    long long int a2;
+    char ch;
+    float b;
+    double b2;
+
+    cin >> a >> a2 >> ch >> b >> b2;
+
+    cout << a << endl;
+    cout << a2 << endl;
+    cout << ch << endl;
+    cout << fixed << setprecision(2) << b << endl;
+    cout << fixed << setprecision(1) << b2 << endl;
+
+    return 0;
+}
