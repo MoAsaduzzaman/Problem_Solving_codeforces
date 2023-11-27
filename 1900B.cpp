b70322ace6a90c595426f520fe4ecafb54937463
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-27 08:55:01 +0600

    Laura and Operations

---
+++ 1900B.cpp
@@ -0,0 +1,35 @@
+//Problem name: 1900B 
+//https://codeforces.com/contest/1900/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+
+        if(abs(b-c)%2==0){
+            cout << 1 << " ";
+        }else{
+            cout << 0 << " ";
+        }
+
+        if(abs(a-c)%2==0){
+            cout << 1 << " ";
+        }else{
+            cout << 0 << " ";
+        }
+
+        if(abs(a-b)%2==0){
+            cout << 1 << " ";
+        }else{
+            cout << 0 << " ";
+        }
+
+        cout << endl;
+    }
+    
+    return 0;
+}
