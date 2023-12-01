a30f822427bb6d143a814c1d37bdefed7bfed00a
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-02 00:05:13 +0600

    Candies and Two Sisters

---
+++ 1335A.cpp
@@ -0,0 +1,18 @@
+//Problem name: 1335A 
+//https://codeforces.com/contest/1335/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n;
+        cin >> n;
+
+        int long long ans = (n-1)/2;
+        
+        cout << ans << endl;
+    }
+    return 0;
+}
