316a2976458b154b152b4891dcf40c57d23b903b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-07 00:03:10 +0600

    Stones on the Table

---
+++ 266A.cpp
@@ -0,0 +1,23 @@
+//Problem name: 266A 
+//https://codeforces.com/problemset/problem/266/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    for(int i=0; i<n; i++){
+        if(s[i]==s[i+1]){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
