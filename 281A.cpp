ad560e67ba1ae643cf7fbd47baf8549cd9d1588c
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-07 00:03:46 +0600

    Word Capitalization

---
+++ 281A.cpp
@@ -0,0 +1,14 @@
+//Problem name: 281A 
+//https://codeforces.com/problemset/problem/281/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+
+    s[0]=toupper(s[0]);
+    cout << s << endl;
+    
+    return 0;
+}
