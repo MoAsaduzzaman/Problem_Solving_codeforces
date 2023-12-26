022424e808468faab29b8634e2a96c8870e0295e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-26 09:58:09 +0600

    Halloumi Boxes

--- 1903A.cpp
+++ 1903A.cpp
@@ -1,28 +1,31 @@
-// Problem name: 1903A 
+// Problem name: 1903A 	
 // https://codeforces.com/contest/1903/problem/A
 
 #include<bits/stdc++.h>
 using namespace std;
+
+string asad(int n, int k){
+    vector <int> a(n);
+
+    for(int i=0; i<n; ++i){
+        cin >> a[i];
+    }
+
+    if(k==1 && !is_sorted(a.begin(), a.end())){
+        return "NO";
+    }else{
+        return "YES";
+    }
+}
+
 int main(){
     int t;
     cin >> t;
-
-    while(t--){
+    while(t){
         int n, k;
         cin >> n >> k;
-
-        vector <int> a(n);
-
-        for (int i=0; i<n; ++i) {
-            cin >> a[i];
-        }
-
-        if(k==1 && !is_sorted(a.begin(), a.end())){
-            cout << "NO" << endl;
-        } else {
-            cout << "YES" << endl;
-        }
+        cout << asad(n,k) << endl;
+        t--;
     }
-
     return 0;
 }
